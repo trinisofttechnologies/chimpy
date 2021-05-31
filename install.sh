8152b74e8d239b218577926bc65056b10baf81ba
Trinisoft Technologies <trinisofttechnologies@gmail.com>
2021-05-31 16:17:35 +0530

    experiment in progress

---
+++ install.sh
@@ -0,0 +1,16 @@
+cd ./node_modules/xolvio-sync-webdriverio
+npm install fibers@^5.0.0 --save
+cd ../..
+
+cd ./node_modules/cucumber
+npm install fibers@^5.0.0 --save
+cd ../..
+
+cd ./node_modules/wdio-sync
+npm install fibers@^5.0.0 --save
+cd ../..
+
+cd ./node_modules/xolvio-fiber-utils
+npm install fibers@^5.0.0 --save
+cd ../..
+
